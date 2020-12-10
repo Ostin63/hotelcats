7e1944a36092de2d7392699386ac0287680936c8
DESKTOP-4LGQ79I\ostin <upssq@mail.ru>
2020-12-10 18:27:18 +0400

    css catalog

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -7,16 +7,36 @@ $(document).ready(function () {
     $('.modal-menu').addClass('d-none');
     $('.modal-overlay').addClass('d-none');
   });
+  $('.form-close').click(function () {
+    $('.modal-menu').addClass('d-none');
+    $('.modal-overlay').addClass('d-none');
+  });
   $('.item').click(function () {
     $('.modal-menu').addClass('d-none');
     $('.modal-overlay').addClass('d-none');
   });
-  $('.filter').click(function () { 
+  $('.left-filter').click(function () { 
     $('.form .modal-form').removeClass('d-none');
     $('.modal-overlay').removeClass('d-none');
   });
-  $('.close').click(function () {
+  $('.form-close').click(function () {
     $('.form .modal-form').addClass('d-none');
     $('.modal-overlay').addClass('d-none');
+  });
+
+  function checkWidth() {
+    let windowWidth = $('body').innerWidth(),
+      overlay = $('.modal-overlay');
+      modalForm = $(".modal-form");
+    if (windowWidth > 1366) {
+      modalForm.removeClass('d-none');
+      overlay.addClass('d-none');
+    } else {
+      modalForm.addClass('d-none');
+    }
+  }
+  checkWidth(); // проверит при загрузке страницы
+  $(window).resize(function () {
+    checkWidth(); // проверит при изменении размера окна клиента
   });
 });
