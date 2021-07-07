3debe966443a9c84f1c7beb032d57ee32fc8a880
Dmitry <upssq@mail.ru>
2021-07-07 13:13:46 +0300

    Добавьте js для фоормы

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -1,46 +1,61 @@
-$(document).ready(function () { 
-  $('.toogle').click(function () {
+$(document).ready(function () {
+  const onAddMenu = () => {
     $('.modal-menu').removeClass('d-none');
     $('.modal-overlay').removeClass('d-none');
-  });
-  $('.close').click(function () {
+  };
+  const onCloseMenu = () => {
     $('.modal-menu').addClass('d-none');
     $('.modal-overlay').addClass('d-none');
-  });
-  $('.form-close').click(function () {
-    $('.modal-menu').addClass('d-none');
-    $('.modal-overlay').addClass('d-none');
-  });
-  $('.item').click(function () {
-    $('.modal-menu').addClass('d-none');
-    $('.modal-overlay').addClass('d-none');
-  });
-  $('.modal-logo').click(function () {
-    $('.modal-menu').addClass('d-none');
-    $('.modal-overlay').addClass('d-none');
-  });
-  $('.left-filter').click(function () { 
+  };
+  const onAddForm = () => {
     $('.form .modal-form').addClass('d-block');
     $('.modal-overlay').removeClass('d-none');
-  });
-  $('.form-close').click(function () {
+  };
+  const onCloseForm = () => {
     $('.form .modal-form').removeClass('d-block');
     $('.modal-overlay').addClass('d-none');
+  };
+  const onAddReservationMenu = () => {
+    $('.modal-reservation').removeClass('d-none');
+    $('.modal-overlay').removeClass('d-none');
+  };
+  const onCloseReservationMenu = () => {
+    $('.modal-reservation').addClass('d-none');
+    $('.modal-overlay').addClass('d-none');
+  };
+  const onAddThankYou = () => {
+    $('.modal-thank-you').removeClass('d-block');
+    $('.modal-overlay').removeClass('d-none');
+  };
+
+  $('.toogle').click(onAddMenu);
+  $('.close').click(onCloseMenu);
+  $('.item').click(onCloseMenu);
+  $('.modal-logo').click(onCloseMenu);
+  $('.left-filter').click(onAddForm);
+  $('.form-close').click(onCloseForm);
+  $('.btn-yellow').click(onCloseForm);
+ 
+  $(window).resize(function () {
+    const win = $(this);
+    if (win.width() >= 1384) {
+      onCloseForm();
+    }
+  });
+
+  $('#filter-form').submit(function (evt) {
+    evt.preventDefault()
+  });
+  $('#form').submit(function (evt) {
+    evt.preventDefault()
   });
 
   // бронь номера
 
-  $('.book').click(function () {
-    $('.modal-reservation').removeClass('d-none');
-    $('.modal-overlay').removeClass('d-none');
-  });
-
-  $('.reservation-close').click(function () {
-    $('.modal-reservation').addClass('d-none');
-    $('.modal-overlay').addClass('d-none');
-  });
+  $('.book').click(onAddReservationMenu);
+  $('.reservation-close').click(onCloseReservationMenu);
   
-  /*$('#form').validate({
+  $('#form').validate({
     rules: {
       fname: {
         required: true,
@@ -61,40 +76,11 @@ $(document).ready(function () {
         checkMask: "Введите полный номер телефона"
       }
     }
-  });*/
+  });
   $('#phone').mask("+7(999)999-9999", {
     autoclear: false
   });
   $(".date").datepicker($.datepicker.regional["ru"]);
 
-  // Спасибо за заявку
-
-  $('#form').validator().on('submit', function (e) {
-    if (!e.isDefaultPrevented()) {
-      $('.modal-reservation').addClass('d-none');
-      $('.modal-thank-you').removeClass('d-none');
-    }
-  });
-  $('.closed').click(function () {
-    $('.modal-thank-you').addClass('d-none');
-    $('.modal-overlay').addClass('d-none');
-  });
-  $('.ok').click(function () {
-    $('.modal-thank-you').addClass('d-none');
-    $('.modal-overlay').addClass('d-none');
-  });
-/*
-  function checkWidth() {
-    let windowWidth = $('body').innerWidth(),
-      overlay = $('.modal-overlay');
-      modalForm = $(".modal-form");
-    if (windowWidth > 1366) {
-      modalForm.removeClass('d-block');
-      overlay.addClass('d-none');
-    }
-  }
-  checkWidth(); // проверит при загрузке страницы
-  $(window).resize(function () {
-    checkWidth(); // проверит при изменении размера окна клиента
-  });*/
 });
+
